c6a0d0e21f921664193f1bd4141f19a06968b31f
Rahul Mishra <robom460@gmail.com>
2021-06-26 19:38:47 +0530

    added meme command and fixed stickied post showing

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -17,7 +17,7 @@ class reddit_scraper(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(description="Fetch programming articles from any programming subreddit")
+    @commands.command(description="Fetch programming articles from any programming subreddit", pass_context = True)
     async def article(self, ctx, *args):
         submission = next(x for x in reddit.subreddit('{}'.format(' '.join(args))).hot() if not x.stickied)
         for submission in reddit.subreddit('{}'.format(' '.join(args))).hot(limit=10):
@@ -25,13 +25,15 @@ class reddit_scraper(commands.Cog):
             em.add_field(name="Upvotes", value=submission.score, inline=False)
             em.add_field(name="Comments", value=submission.num_comments, inline=False)
             await ctx.send(embed = em)
+            await ctx.message.delete()
 
-    @commands.command(description="fetch memes from any subreddit")
+    @commands.command(description="fetch memes from any subreddit", pass_context = True)
     async def meme(self, ctx, *args):
         submission = reddit.subreddit('{}'.format(' '.join(args))).random()
         em = discord.Embed(title=submission.title, description=submission.selftext[:2048], url = submission.url, color=0xFF4301)        
         em.set_image(url=submission.url)
         await ctx.send(embed = em)
+        await ctx.message.delete()
 
    
 
