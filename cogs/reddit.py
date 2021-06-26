b236ec41d042808d2e6247a69523eecfdbe6f901
Rahul Mishra <robom460@gmail.com>
2021-06-26 19:37:20 +0530

    added meme command and fixed stickied post showing up everytime articles were requested

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -3,6 +3,7 @@ import praw
 from dotenv import load_dotenv
 import os
 import discord
+from praw.models.listing.mixins import submission
 
 load_dotenv()
 
@@ -16,29 +17,23 @@ class reddit_scraper(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def python(self, ctx):
-        for submission in reddit.subreddit("python").hot(limit=10):
-            em = discord.Embed(title=submission.title, description=submission.selftext[:2048], url = submission.url, color=0xFF4301)
+    @commands.command(description="Fetch programming articles from any programming subreddit")
+    async def article(self, ctx, *args):
+        submission = next(x for x in reddit.subreddit('{}'.format(' '.join(args))).hot() if not x.stickied)
+        for submission in reddit.subreddit('{}'.format(' '.join(args))).hot(limit=10):
+            em = discord.Embed(title=submission.title, description=submission.selftext[:2048], url = submission.url, color=0xFF4301)        
             em.add_field(name="Upvotes", value=submission.score, inline=False)
             em.add_field(name="Comments", value=submission.num_comments, inline=False)
             await ctx.send(embed = em)
 
-    @commands.command()
-    async def react(self, ctx):
-        for submission in reddit.subreddit("react").hot(limit=10):
-            em = discord.Embed(title=submission.title, description=submission.selftext[:2048], url = submission.url, color=0xFF4301)
-            em.add_field(name="Upvotes", value=submission.score, inline=False)
-            em.add_field(name="Comments", value=submission.num_comments, inline=False)
-            await ctx.send(embed = em)
+    @commands.command(description="fetch memes from any subreddit")
+    async def meme(self, ctx, *args):
+        submission = reddit.subreddit('{}'.format(' '.join(args))).random()
+        em = discord.Embed(title=submission.title, description=submission.selftext[:2048], url = submission.url, color=0xFF4301)        
+        em.set_image(url=submission.url)
+        await ctx.send(embed = em)
 
-    @commands.command()
-    async def learn_programming(self, ctx):
-        for submission in reddit.subreddit("learnprogramming").hot(limit=10):
-            em = discord.Embed(title=submission.title, description=submission.selftext[:2048], url = submission.url, color=0xFF4301)
-            em.add_field(name="Upvotes", value=submission.score, inline=False)
-            em.add_field(name="Comments", value=submission.num_comments, inline=False)
-            await ctx.send(embed = em)
+   
 
 def setup(bot):
         bot.add_cog(reddit_scraper(bot))
