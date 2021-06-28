aa34e007130429802d066c732d170c44b7c76f4c
Supreme-Watermelon <robom460@gmail.com>
2021-06-28 19:07:49 +0530

    added sorting articles  by week/month/year/all time to dev scraper

--- cogs/dev.py
+++ cogs/dev.py
@@ -15,11 +15,57 @@ class dev_scraper(commands.Cog):
         self.bot=bot
 
     @commands.command()
-    async def dev(self,ctx):
+    async def dev_latest(self,ctx):
+        url = "https://dev.to/latest"
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content, 'html.parser')
         articles = soup.find('div', class_='crayons-layout crayons-layout--3-cols crayons-layout--3-cols--drop-right-left')
-        links = articles.find_all('a', class_='crayons-link crayons-link--contentful')
+        links = articles.find_all('a', class_='crayons-story__hidden-navigation-link')
         for url in links:
             await ctx.send("https://dev.to"+url.get('href'))
+
+    @commands.command()
+    async def dev_week(self,ctx):
+        url = "https://dev.to/top/week"
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        articles = soup.find('div', class_='crayons-layout crayons-layout--3-cols crayons-layout--3-cols--drop-right-left')
+        links = articles.find_all('a', class_='crayons-story__hidden-navigation-link')
+        for url in links:
+            await ctx.send("https://dev.to"+url.get('href'))
+    
+    @commands.command()
+    async def dev_month(self,ctx):
+        url = "https://dev.to/top/month"
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        articles = soup.find('div', class_='crayons-layout crayons-layout--3-cols crayons-layout--3-cols--drop-right-left')
+        links = articles.find_all('a', class_='crayons-story__hidden-navigation-link')
+        for url in links:
+            await ctx.send("https://dev.to"+url.get('href'))
+     
+    @commands.command()
+    async def dev_year(self,ctx):
+        url = "https://dev.to/top/year"
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        articles = soup.find('div', class_='crayons-layout crayons-layout--3-cols crayons-layout--3-cols--drop-right-left')
+        links = articles.find_all('a', class_='crayons-story__hidden-navigation-link')
+        for url in links:
+            await ctx.send("https://dev.to"+url.get('href'))
+    
+    @commands.command()
+    async def dev_infinity(self,ctx):
+        url = "https://dev.to/top/infinity"
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        articles = soup.find('div', class_='crayons-layout crayons-layout--3-cols crayons-layout--3-cols--drop-right-left')
+        links = articles.find_all('a', class_='crayons-story__hidden-navigation-link')
+        for url in links:
+            await ctx.send("https://dev.to"+url.get('href'))
+
+
+
 
 
 def setup(bot):
